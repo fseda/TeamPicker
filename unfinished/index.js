31eb819f485ca2469b16ffdf3824d7e5cbadd160
Felipe Seda <fsra1610@gmail.com>
2021-02-17 14:40:55 -0300

    some fixes

--- unfinished/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app     = express();
-
-require('./startup/routes')(app);
-
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => winston.info(`Listenig on port ${port}...`));
-
-module.exports = server;
--- unfinished/startup/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const teams = require('../routes/teams');
-
-module.exports = function(app) {
-  app.use(express.json());
-  app.use('/api/teams', teams);
-}
